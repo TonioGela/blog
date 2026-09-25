d2debba774b8b205c2c42c170fb1b31c18ebb4e3
Antonio Gelameris <toniogela89@gmail.com>
2026-09-25 15:17:11 +0200

    wip

--- shell.nix
+++ shell.nix
@@ -1,7 +1,6 @@
 let
   pins = import ./npins;
   pkgs = import pins.nixpkgs-unstable { };
-  # zola serve, with drafts on: local is where you want to see them
   serve = pkgs.writeShellScriptBin "serve" ''exec zola serve --drafts "$@"'';
 in
 pkgs.mkShellNoCC {
